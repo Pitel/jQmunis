a0f80c36581e8a3d7b3ce990d78e252d7ce0fc7d
Bc. Jan Kaláb <pitlicek@gmail.com>
2011-01-20 09:02:15 +0100

    Working draft of the script. It needs some touches, but it works.

---
+++ jQmunis.js
@@ -0,0 +1,56 @@
+var otazky = ["How old are you?",
+	"Where do you live?",
+	"What is your name?"
+];
+
+var odpovedi = Array();
+odpovedi[1] = ["0--50", "50--100"];
+odpovedi[2] = ["Europe", "USA", "Mars"];
+
+$(function() {
+	//Hlavicka (otazky)
+	var row = $("<tr/>");
+	$.each(otazky, function(index, value) {
+		var cell = $("<th/>");
+		cell.text(value);
+		cell.appendTo(row);
+	});
+	$("table").append(row);
+	
+	// Odpovedi
+	$.get("data.xml", function(xml) {
+		//console.log(xml);
+		//console.log($("VERZE", xml).text());
+		
+		var otazek = [];
+		$("ODP", xml).each(function() {
+			otazek.push(parseInt($(this).attr("POR")));
+		});
+		//console.log(otazek);	//Jeste pole
+		otazek = otazek.sort(function(a, b) {return b - a})[0];
+		//console.log(otazek);	//Pocet otazek
+		
+		//console.log($("PROCHAZEJICI:not([UCO])", xml).length);	//Pocet pruchodu (vcetne prazdnych!)
+		$("PROCHAZEJICI:not([UCO])", xml).each(function() {
+			var row = $("<tr/>");
+			for (var i = 1; i <= otazek; i++) {
+				//console.log(i, $("AKCE:last ODP[POR='" + i + "']", this));
+				var odpovedi_arr = Array();
+				$("AKCE:last ODP[POR='" + i + "']", this).each(function() {
+					var odpoved = $(this).attr("HOD");
+					if (odpovedi[i] && !isNaN(odpoved)) {
+						odpovedi_arr.push(odpovedi[i][parseInt(odpoved) - 1]);
+					} else {
+						odpovedi_arr.push(odpoved);
+					}
+				});
+				var cell = $("<td/>");
+				cell.text(odpovedi_arr.join(", "))
+				cell.appendTo(row);
+			}
+			if (row.text()) {	//Vyhodime prazdne odpovedi
+				$("table").append(row);
+			}
+		});
+	}, "xml");	//Format
+});
